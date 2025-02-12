ac97598b3688605a36c3425768f5a93caaff92b1
dslrsiddesh <siddeshdslr@gmail.com>
2025-02-12 23:40:07 +0530

    update user service

--- JEDI_FLIPKART_CODEBASE/src/com/flipkart/business/UserService.java
+++ JEDI_FLIPKART_CODEBASE/src/com/flipkart/business/UserService.java
@@ -1,22 +1,8 @@
 package com.flipkart.business;
 
 import com.flipkart.bean.User;
-import com.flipkart.bean.*;
-import java.util.List;
-import java.util.ArrayList;
 
 public class UserService {
-
-    public User registerUser(String name, String contact, String email, String password, String role) {
-        User user = new User();
-        user.setName(name);
-        user.setContact(contact);
-        user.setEmail(email);
-        user.setPassword(password);
-        user.setRole(role);
-        return user;
-    }
-
     public User loginUser(String email, String password) {
         System.out.println("User with email " + email + " logged in successfully!");
         return null;
