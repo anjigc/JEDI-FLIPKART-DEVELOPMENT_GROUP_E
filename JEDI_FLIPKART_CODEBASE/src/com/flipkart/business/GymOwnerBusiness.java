1d2bbf3abf5abe366a8959a195249c0b456c92a4
msrisujan <mbsrisujan@gmail.com>
2025-02-12 16:32:33 +0530

    add gymownerbusiness

---
+++ JEDI_FLIPKART_CODEBASE/src/com/flipkart/business/GymOwnerBusiness.java
@@ -0,0 +1,17 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.Gym
+
+public class GymOwnerBusiness {
+    public boolean addGym(Gym gym, GymOwner gymOwner) {
+        // Logic to add a new gym
+    }
+
+    public boolean updateGym(Gym gym) {
+        // Logic to update gym details
+    }
+
+    public List<Gym> getGymsByOwnerId(int ownerId) {
+        // Logic to get list of gyms by owner ID
+    }
+}
