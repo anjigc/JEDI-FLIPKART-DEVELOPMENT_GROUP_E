2826eda24cc1803a94fa28193bd23a4f158c996b
anjigc <anjitachandoragargi@gmail.com>
2025-02-12 14:57:55 +0530

    changed some files

--- Task4/src/com/flipkart/bean/User.java
+++ Task4/src/com/flipkart/bean/User.java
@@ -1,4 +1,52 @@
-package com.flipkart.bean;
+package main.java.com.flipkart.bean;
+
 
 public class User {
+
+    private int id;
+    private String name;
+    private String mobileNo;
+    private String email;
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
