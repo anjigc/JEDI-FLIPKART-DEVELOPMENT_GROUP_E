ac7e8e01545e9f0ae23685e33d9407a4fe5e3cc2
anjigc <anjitachandoragargi@gmail.com>
2025-02-12 14:47:22 +0530

    new commit

---
+++ Task4/src/main/java/com/flipkart/bean/GymOwner.java
@@ -0,0 +1,49 @@
+package main.java.com.flipkart.bean;
+
+public class GymOwner {
+    private int id;
+    private String name;
+    private String email;
+    private String aadhaarProof;
+    private String panNo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAadhaarProof() {
+        return aadhaarProof;
+    }
+
+    public void setAadhaarProof(String aadhaarProof) {
+        this.aadhaarProof = aadhaarProof;
+    }
+
+    public String getPanNo() {
+        return panNo;
+    }
+
+    public void setPanNo(String panNo) {
+        this.panNo = panNo;
+    }
+}
