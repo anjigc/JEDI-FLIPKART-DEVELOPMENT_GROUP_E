b5b35dbb4938e25fde7c6a37316b80769a190e5e
rohit.pansari9 <rohit.pansari9@gmail.com>
2025-02-16 01:59:59 +0530

    dao changes

---
+++ JEDI_FLIPKART_CODEBASE/src/com/flipkart/utils/FlipFitDBConnection.java
@@ -0,0 +1,22 @@
+package com.flipkart.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FlipFitDBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/FlipFitDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "YOUR_PASSWORD_HERE";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database connection failed!");
+        }
+    }
+}
+
+
