de71cd69fe421c7395b0c8a56a15a6df4fd56e39
rohit.pansari9 <rohit.pansari9@gmail.com>
2025-02-16 02:00:42 +0530

    sql constants improved

--- JEDI_FLIPKART_CODEBASE/src/com/flipkart/utils/FlipFitDBConnection.java
+++ JEDI_FLIPKART_CODEBASE/src/com/flipkart/utils/FlipFitDBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class FlipFitDBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/FlipFitDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "YOUR_PASSWORD_HERE";
+    private static final String PASSWORD = "ENTER_YOUR_PASSWORD_HERE";
 
     public static Connection getConnection() {
         try {
