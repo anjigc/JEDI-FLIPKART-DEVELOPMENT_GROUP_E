f8ddd67176384b5fd7066f35eb4e6a4e60494dec
sakshi2407 <lcs2021020@iiitl.ac.in>
2025-02-17 16:15:43 +0530

    documentation added

--- JEDI_FLIPKART_CODEBASE/src/com/flipkart/utils/FlipFitDBConnection.java
+++ JEDI_FLIPKART_CODEBASE/src/com/flipkart/utils/FlipFitDBConnection.java
@@ -13,7 +13,7 @@ public class FlipFitDBConnection {
     // Database connection details
     private static final String URL = "jdbc:mysql://localhost:3306/FlipFitDB";
     private static final String USER = "root";
-    private static final String                                                                                                                                 PASSWORD = "FL1P!S@k#0703";
+    private static final String                                                                                                                                 PASSWORD = " ";
 
     /**
      * Establishes a connection to the FlipFit database.
